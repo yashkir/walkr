ce16b88ec9c4698ad4f7350c834938e46b21da6b
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-05-25 12:09:16 -0400

    make intial Forums models: Forum, Thread, Post

--- forums/models.py
+++ forums/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Forum(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+
+
+class Thread(models.Model):
+    forum = models.ForeignKey(Forum, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    title = models.CharField(max_length=64)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Post(models.Model):
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    position = models.PositiveIntegerField()
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
