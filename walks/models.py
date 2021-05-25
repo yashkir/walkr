983a1b2a92733b98762a4d74f0fb661ed1d3a80d
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-05-24 22:41:43 -0400

    create Models: Walk, Stop, Picture

--- walks/models.py
+++ walks/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Walk(models.Model):
+    '''A full guided tour.'''
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    is_public = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title}: {self.description[:25]}"
+
+
+class Stop(models.Model):
+    '''A location that is part of a Walk.'''
+    walk = models.ForeignKey(Walk, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    location_text = models.TextField()
+    order = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return f"{self.title} ({self.location_text}): {self.description[:25]}"
+
+
+class Picture(models.Model):
+    '''An illustration of a Stop.'''
+    stop = models.ForeignKey(Stop, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+    image = models.ImageField()
+
+    def __str__(self):
+        return f"{self.title}"
