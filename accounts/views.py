171e00c8a5e41de1cc7f30e42682f6a9010a2b95
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-05-24 21:59:09 -0400

    add LoginRequired on accounts/profile/

--- accounts/views.py
+++ accounts/views.py
@@ -3,9 +3,10 @@ from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class Profile(TemplateView):
+class Profile(LoginRequiredMixin, TemplateView):
     template_name = 'accounts/profile.html'
 
 class Register(CreateView):
