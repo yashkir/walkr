feb125f9c8ae4437fe127923cba6e9c0762ff4ca
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-05-25 12:56:25 -0400

    register Thread and Post with admin

--- forums/admin.py
+++ forums/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Forum
+from .models import Forum, Thread, Post
 
 
 admin.site.register(Forum)
+admin.site.register(Thread)
+admin.site.register(Post)
